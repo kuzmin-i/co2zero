193e354835407fdd7487e91a6bfe42eaa2a7dd8b
Ilya Kuzmin <68997547+kuzmin-i@users.noreply.github.com>
2021-07-13 16:55:35 +0300

    dd

--- src/components/map/mapblock.js
+++ src/components/map/mapblock.js
@@ -1,29 +1,44 @@
-import mapboxgl from 'mapbox-gl';
+import React, {useEffect, useState, useRef, useCallback} from 'react'
 
-import React, {useEffect, useState, useRef} from 'react'
+import DeckGL from '@deck.gl/react';
+import {ScatterplotLayer} from '@deck.gl/layers';
+import ReactMapGL from 'react-map-gl';
 
-mapboxgl.accessToken = "pk.eyJ1IjoibWFya2thYmllcnNraSIsImEiOiJja2lpa3N2c3QwaXVrMnltbHVzcXZ3dDU2In0.t_Lcd-0hPAJSk75HCJFw0g";
+import {MapboxLayer} from '@deck.gl/mapbox';
+
+const INITIAL_VIEW_STATE = {
+  longitude: -74.50,
+  latitude: 40,
+  zoom: 9
+};
+
+const MAPBOX_TOKEN = "pk.eyJ1IjoibWFya2thYmllcnNraSIsImEiOiJja2lpa3N2c3QwaXVrMnltbHVzcXZ3dDU2In0.t_Lcd-0hPAJSk75HCJFw0g"
+
+const data = [
+  {position: [-74.5, 40], size: 100}
+];
 
 const MapBlock = () => {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(51.073);
-    const [lat, setLat] = useState(45);
-    const [zoom, setZoom] = useState(5.91); 
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [lng, lat],
-        zoom: zoom
-        });
-    });
+  const [viewport, setViewport] = useState({
+    style: 'mapbox://styles/mapbox/streets-v11',
+    width: '100%',
+    height: '100%',
+    latitude: 50.209,
+    longitude: 143.387,
+    zoom: 5.91
+  });
 
-    return(
-        <div ref={mapContainer} style={{height: '600px'}} className="map-container map" />
-    )
+  return (
+    <div className="map">
+        <ReactMapGL
+            {...viewport}
+            mapboxApiAccessToken={ MAPBOX_TOKEN }
+            onViewportChange={nextViewport => setViewport(nextViewport)}
+        />
+        
+    </div>
+  );
 
 }
 
